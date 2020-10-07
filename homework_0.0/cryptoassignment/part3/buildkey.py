85f6072f1d4dd9be76af09b3922e1e01ea9bee55
zakattack9 <zakattack26111@gmail.com>
2020-10-06 17:30:07 -1000

    added hw1

--- homework_0.0/cryptoassignment/part3/buildkey.py
+++ homework_0.0/cryptoassignment/part3/buildkey.py
@@ -8,7 +8,8 @@ import fileinput
 def wrap64(string):
     return '\n'.join(textwrap.wrap(string,64))
 
-bitsknownint = 0
+bitsknownint = ""
+curr = 255
 
 for line in fileinput.input():
     if len(line) > 10:
@@ -16,38 +17,32 @@ for line in fileinput.input():
         if (len(line) % 2 == 0):
             firstline = '0' + line
         continue
-    x, y = map(int, line.split())
+    x, y = map(int, line.split()) #
     hint = ((x - 1) << 7) | (y - 32) # AES key fully shifted left
     tryabit = hint & 1 # last bit popped off
     i = hint >> 1 # shift AES key to the right
 # All you have to do is figure out what YYY and ZZZ should be...
     printStr = "HINT: " + str(hint) + " TRYABIT: " + str(tryabit) + " AT I: " + str(i)
-    print(printStr)
+    # print(printStr)
+    print(str(i) + " " + str(tryabit))
     # print(str(x) + " " + str(y))
     # print(firstline)
-    # print(tryabit)
-    # print(i)
     # if (tryabit & 1 == ):
     # if (len(str(bitsknownint)) - 1 < 256):
-    # if ()
-        # bitsknownint = str(tryabit) + str(bitsknownint)
+    if (i < curr or tryabit == 1):
+        bitsknownint = str(tryabit) + str(bitsknownint)
+        curr = curr - 1
         # bitsknownint = ((tryabit << (len(str(bitsknownint)))) | bitsknownint) 
         # print(bitsknownint)
-# 00100 >> 0010
-# 0111 
-
-# 510 => 111111110
-# 508 => 111111100
-# 509 => 111111101
-# 506 => 111111010
 
 #while (len(bitsknown) < 256):
 #    i = 255 - len(bitsknown)
     #for tryabit in ['0', '1']:
         #hint = (i << 1) + int(tryabit)
                 #bitsknown = str(tryabit) + bitsknown
-bitsknownint = int(bitsknownint[:-1], 2)
+bitsknownint = int(bitsknownint, 2)
 print(bitsknownint)
+# print(len(bitsknownint))
 AESkey = long_to_bytes(bitsknownint, 32)
 aes = AESCipher(AESkey)
 print(firstline)
@@ -55,6 +50,10 @@ print(len(firstline))
 eavesdroppedaes = bytes.fromhex(firstline)
 #('088d72fda25863ae81a27ddc286ee8ffef55bdcd0eeee4487fa42cb9c012155e6c38a32d741c68aaa86fda4c9878cbb4')
 print('Recovered plaintext is: {}'.format(aes.decrypt(eavesdroppedaes)[:400]))
+
+with open('rsaplaintext.txt', 'wb') as out_file:
+  out_file.write(aes.decrypt(eavesdroppedaes)[:400])
+
 printme = "{0:b}".format(bitsknownint)
 while len(printme) < 256:
     printme = '0' + printme
