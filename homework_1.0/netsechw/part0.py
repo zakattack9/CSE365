d851c75a8cf1e931fc20306779dd66818fa932e2
zakattack9 <zakattack26111@gmail.com>
2020-10-08 13:22:59 -1000

    added explanation for prob 8

--- homework_1.0/netsechw/part0.py
+++ homework_1.0/netsechw/part0.py
@@ -15,21 +15,7 @@ for p in cap:
       seq_nums[p.tcp.seq_raw] = 1
 print(seq_nums)
 
-
-
-# check if machine responds with ICMP port unreachable
-# check if the same machine does not respond with port unreachable for port 69 (meaning port is open)
-
-# (p.tcp.seq_raw) # for sequence nums
-# 132.222.172.2* (neither were called); has calls to destination port 69 (TFTP)
-# 150.114.84.2
-# 150.114.84.1
-# 4.240.20.1* (neither were called); has calls to destination port 69 (TFTP)
-# 188.136.176.1
-# 188.136.176.2
-
-# 188.136.176.3 (returned ICMP port unreachable for other ports but gave no response for port 69—likely an open port)
-
+# QUESTION 6
 # raw sequence numbers frequency table
 # {
 #   "1036411689": 1022,
@@ -53,3 +39,24 @@ print(seq_nums)
 #   "190739001": 770,
 #   "190804536": 727
 # }
+
+# QUESTION 8
+# check if machine responds with ICMP port unreachable
+# check if the same machine does not respond with port unreachable for port 69 (meaning port is open)
+#
+# (udp.port == 69) && ip.dst == 169.125.5.2 && (icmp >= "Destination unreachable")
+# above Wireshark query will find all UDP machines that sent back ICMP packets to attacker's port 69
+# compile a list of distinct source IP's that sent back ICMP packets; indicates machine is online
+#
+# ip.dst == 169.125.5.2 && (icmp >= "Destination unreachable")
+# above Wireshark query will find all UDP machines that sent back ICMP packets
+# compare list of distinct source IP's that sent back ICMP packets to the list of IP's that sent ICMP packets to port 69 of attacker
+# IP addresses that don't send back ICMP packets for port 69 but did send back other ICMP packets for other ports indicates that port 69 is open
+#
+# 132.222.172.2* (neither were called); has calls to destination port 69 (TFTP)
+# 150.114.84.2
+# 150.114.84.1
+# 4.240.20.1* (neither were called); has calls to destination port 69 (TFTP)
+# 188.136.176.1
+# 188.136.176.2
+# 188.136.176.3 (returned ICMP port unreachable for other ports but gave no response for port 69; likely an open port for TFTP)
